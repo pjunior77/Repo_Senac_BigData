58966cc0727e22c2d6658a17f3a6cb2b1a4f3664
PAULO FREDERICO DE FIGUEIREDO MONTEIRO JUNIOR <paulo.frederico@aluno.senac.rio>
2025-09-01 21:31:21 -0300

    Atualização de código.

--- Aula04_ListaComEstruturaRepeticao.py
+++ Aula04_ListaComEstruturaRepeticao.py
@@ -7,5 +7,26 @@ while True:
     except:
         print('Valor inválido. Digite um número inteiro.')
 
+#Crio as listas
+nomeProduto = []
+valorProduto = []
+
+#Para cada produto pega as informações
 for i in range(0, qtdeProdutos, 1):
-    print('FIM')
+    
+    nome = input(f'Qual o nome do produto {i+1}? ').strip()
+    nomeProduto.append(nome)
+
+    while True:
+        try:
+            valor = float(input(f'Qual o valor do produto {i+1}? R$ '))
+            valorProduto.append(valor)
+            break
+        except:
+            print('Valor inválido. Digite um número.')
+
+#Imprime os resultados
+print('\n----- Exibindo a lista de produtos -----')
+for i in range(0, qtdeProdutos, 1):
+    print(f'Nome do produto {i+1}: {nomeProduto[i]} - Valor do produto {i+1}: R$ {valorProduto[i]:.2f}')
+print('--- FIM ---')
